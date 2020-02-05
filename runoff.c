0fa333fabd9e8490cc07e763d0c7150b715e7277
mattshirvan <mattshirvan@users.noreply.github.com>
2020-02-05 19:16:25 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -190,11 +190,14 @@ int find_min(void)
 bool is_tie(int min)
 {
     // TODO
-    for (int i = 0; i < candidate_count; i++)
+    if (candidates[i].eliminated == false)
     {
-        if (candidates[i].votes == min && candidates[i].eliminated == false)
+        for (int i = 0; i < candidate_count; i++)
         {
-            return true;
+            if (candidates[i].votes == min && candidates[i].eliminated == false)
+            {
+                return true;
+            }
         }
     }
     return false;
