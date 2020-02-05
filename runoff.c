a4cf4008e257b5774bf3ca67f8de43c621ecc8ac
mattshirvan <mattshirvan@users.noreply.github.com>
2020-02-05 17:35:29 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -135,7 +135,7 @@ bool vote(int voter, int rank, string name)
         {
             if (strcmp(candidates[j].name, name) == 0)
             {
-                preferences[i][j] = j;
+                preferences[i][j] = rank;
                 return true;
             }
         }
