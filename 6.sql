e591a081eb1c1b99ee8d1f66ff20f6e80cab3b00
mattshirvan <mattshirvan@users.noreply.github.com>
2020-03-14 22:05:14 +0000

    automated commit by submit50

--- 6.sql
+++ 6.sql
@@ -1,2 +1,6 @@
-SELECT ROUND(AVG(rating) , 2) FROM ratings WHERE (SELECT year FROM movies WHERE year = 2012);
+-- SELECT ROUND(AVG(rating), -2) FROM ratings WHERE (SELECT year FROM movies WHERE year = 2012);
+
+SELECT printf("%.2f", AVG(rating)) FROM ratings WHERE (SELECT year FROM movies WHERE year = 2012);
+
+
 
