ab120c9f4c94a8281addeb2aaf8f9985014ddb89
mattshirvan <mattshirvan@users.noreply.github.com>
2020-03-14 00:58:33 +0000

    automated commit by submit50

--- 10.sql
+++ 10.sql
@@ -3,3 +3,4 @@ FROM people
 JOIN directors ON people.id = directors.person_id
 JOIN movies ON movies.id = directors.movie_id
 WHERE (SELECT ratings.rating AS rating FROM ratings WHERE rating >= 9.0);
+
--- 6.sql
+++ 6.sql
@@ -1,1 +1,2 @@
-SELECT AVG(rating) FROM ratings WHERE (SELECT year FROM movies WHERE year = 2012);
+SELECT ROUND(AVG(rating) , 2) FROM ratings WHERE (SELECT year FROM movies WHERE year = 2012);
+
