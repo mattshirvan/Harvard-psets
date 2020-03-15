f095682b8a48b971076f9895f2043bfd937fbd0f
mattshirvan <mattshirvan@users.noreply.github.com>
2020-03-15 04:47:04 +0000

    automated commit by submit50

---
+++ roster.py
@@ -0,0 +1,22 @@
+from sys import argv, exit
+import cs50
+
+# ensure correct command line arguments
+if len(argv) != 2:
+    print("Usage: python roster.py Gryffindor")
+    exit(0)
+
+# open the database
+db = cs50.SQL("sqlite:///students.db")
+
+# set house from command line
+house = argv[1]
+
+# query the database for student roster
+roster = db.execute("SELECT first, middle, last, birth FROM students WHERE house = ? ", house)
+
+# print house roster
+for row in roster:
+    if row["middle"] != None:
+        print(row["first"], row["middle"], row["last"] + ", born", row["birth"])
+    print(row["first"], row["last"] + ", born", row["birth"])
